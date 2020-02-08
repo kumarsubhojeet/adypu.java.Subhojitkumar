e7c6e10630b731570373b1f647faa84b6105d8d9
kumarsubhojeet <60726609+kumarsubhojeet@users.noreply.github.com>
2020-02-09 01:22:08 +0530

    Keywords

---
+++ Keyword/ThisKey.java
@@ -0,0 +1,23 @@
+package keywords;
+
+public class ThisKey {
+	 int id;
+     String name;    
+     ThisKey(int id,String name) { 
+      this.id=id;
+      this.name=name;
+      System.out.println("constructor body : id : "+id+"\nName : "+name);
+     }
+     private void showData() {
+     System.out.println("id : "+id+"\nName : "+name);
+     }
+     
+     public static void main(String[] args) {
+        ThisKey key=new ThisKey(1,"abcd");
+        key.showData();
+}
+     
+}
+
+    © 2020 GitHub, Inc.
+    Terms
