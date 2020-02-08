3b5f38b72b0be94334959c5cc3e4da7768754e62
kumarsubhojeet <60726609+kumarsubhojeet@users.noreply.github.com>
2020-02-09 01:17:50 +0530

    Abstraction

---
+++ Abstraction/CheckAbstraction.java
@@ -0,0 +1,19 @@
+lines (14 sloc) 396 Bytes
+package abstraction;
+
+public class CheckAbstraction extends AbstractDemo implements InterfaceDemo {
+    @Override//annotation
+    void showData() {
+        System.out.println("inside show data.");
+    }
+
+    public static void main(String[] args) {
+        CheckAbstraction abstraction=new CheckAbstraction();
+        abstraction.showData();
+    }
+
+    @Override
+    public void add() {
+
+    }
+}
